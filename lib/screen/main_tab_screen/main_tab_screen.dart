d9cca7a68ab31be6221e63a89ada51cbf7be64bd
kko20s25095-ui <kko20_s25095@gclass.ice.go.kr>
2026-09-25 13:28:09 +0900

    각 탭들 관리 화면 추가

---
+++ lib/screen/main_tab_screen/main_tab_screen.dart
@@ -0,0 +1,56 @@
+import 'package:dummyjson/app_color/app_color.dart';
+import 'package:flutter/material.dart';
+
+class MainTabScreen extends StatefulWidget {
+  const MainTabScreen({super.key});
+
+  @override
+  State<MainTabScreen> createState() => _MainTabScreenState();
+}
+
+class _MainTabScreenState extends State<MainTabScreen> {
+  // 검색어 컨트롤러
+  final TextEditingController _searchCtrl = TextEditingController();
+
+  //화면 관리 변수
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+          index: _currentIndex,
+          children: [
+
+          ]
+      ),
+
+      //바텀 네비게이션 바
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+
+        type: BottomNavigationBarType.fixed,
+
+        //클릭 시 리빌드 처리
+        onTap: (value) {
+          setState(() {
+            _currentIndex = value;
+          });
+        },
+
+        //클릭 시 각 탭 효과 처리
+        selectedItemColor: AppColor.primary,
+        selectedFontSize: 14,
+        unselectedFontSize: 12,
+
+
+        items: [
+          BottomNavigationBarItem(label: "홈", icon: Icon(Icons.home_outlined)),
+          BottomNavigationBarItem(label: "장바구니", icon: Icon(Icons.shopping_bag_outlined)),
+          BottomNavigationBarItem(label: "위시 리스트", icon: Icon(Icons.favorite_border_rounded)),
+          BottomNavigationBarItem(label: "마이 페이지", icon: Icon(Icons.person)),
+        ],
+      ),
+    );
+  }
+}
